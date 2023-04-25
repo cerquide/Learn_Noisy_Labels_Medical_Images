42b7bb48b0f804d3947d6bccbd9d2d971696595e
Georgios <gathanasiou@iiia.csic.es>
2023-04-25 16:56:11 +0200

    ooc

--- Models.py
+++ Models.py
@@ -342,7 +342,8 @@ def double_conv(in_channels, out_channels, step, norm):
             nn.PReLU(),
             nn.Conv2d(out_channels, out_channels, 3, stride=1, padding=1, groups=1, bias=False),
             nn.InstanceNorm2d(out_channels, affine=True),
-            nn.PReLU()
+            nn.PReLU(),
+            nn.MaxPool2d((2, 2))
         )
     elif norm == 'bn':
         return nn.Sequential(
