051753eb3c273a6bf6a4f710039fd046e76b1878
Georgios <gathanasiou@iiia.csic.es>
2023-04-27 16:42:40 +0200

    tf to pt

--- tf_coc3_model.py
+++ tf_coc3_model.py
@@ -39,7 +39,7 @@ val_split = 0.05
 epochs = 10
 patience = 500
 
-TL = False
+TL = True
 weights_path = './tf_coc/coc_Final_dict.pt'
 
 def train_model(images_path:Path, masks_path:Path, path_to_save: Path, log_path:Path):
