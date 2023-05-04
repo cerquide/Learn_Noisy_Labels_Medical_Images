7f63856b27345b11aa2295c8e8213cfbd4f3fcc8
Georgios <gathanasiou@iiia.csic.es>
2023-05-04 23:22:54 +0200

    tf to pt

--- tf_utils.py
+++ tf_utils.py
@@ -59,6 +59,7 @@ def save_borders(boolean, tensor, annotator = 1):
 
     for i in range(tensor.shape[0]):
 
+        plt.imshow(borders[i])
         plt.savefig(f'./tf_coc3/wtTL/borders/border_{i}_annotator_{annotator}.png')
         plt.clf()
 
