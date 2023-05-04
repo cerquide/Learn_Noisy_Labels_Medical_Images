ce6784540a3a305bfd5221b9111ae29077b42514
Georgios <gathanasiou@iiia.csic.es>
2023-05-04 17:20:39 +0200

    tf to pt

--- tf_utils.py
+++ tf_utils.py
@@ -209,7 +209,7 @@ def calculate_cm(y_pred, y_true):
     col_sums = confusion_matrix.sum(dim = 0)
     confusion_matrix = confusion_matrix / col_sums
 
-    return confusion_matrix.round(4) * 100
+    return torch.round(confusion_matrix * 10000) / 10000 * 100
    
 def evaluate_cm(pred, pred_cm, true_cm):
 
