8ed43400bbe568c4f0aea1ba46fa9f11bf37af41
Georgios <gathanasiou@iiia.csic.es>
2023-05-05 18:07:30 +0200

    tf to pt

--- tf_utils.py
+++ tf_utils.py
@@ -163,8 +163,10 @@ def noisy_loss(pred, cms, labels, names):
     # print(focus_pred[0].size())
 
     enum = 0
+    total_loss = 0
     for cm, label in zip(cms, focus_labels):
 
+        batch_loss = 0 
         for i in range(len(focus_pred)):
         
             cm_simple = cm[i, :, :, 0, 0].unsqueeze(0).unsqueeze(-1).repeat(1, 1, 1, focus_pred[i].size(2)).to('cuda')
@@ -186,10 +188,14 @@ def noisy_loss(pred, cms, labels, names):
             print(label[i].size())
 
             loss_current = dice_loss2(pred_noisy_mask.to(DEVICE), label[i].to(DEVICE))
-            print(loss_current)
+            print(loss_current.item())
 
-            return 0
+            batch_loss += loss_current
+        batch_loss = batch_loss / (i + 1)
+        total_loss += batch_loss
 
+        print(total_loss)
+        
     # clear, dirty = clear_pred(pred_norm)
 
     # indices = (dirty == True).nonzero()
